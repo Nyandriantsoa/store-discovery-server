fd4d46f92cb9bfdc33b7f56c10562e9e438759f5
Ny ANDRIANTSOA <nyandriantsoa25@gmail.com>
2020-10-28 18:05:03 -0400

    discovery server initialized

---
+++ src/main/java/com/imamiako/storediscoveryserver/StoreDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.imamiako.storediscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class StoreDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreDiscoveryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imamiako/storediscoveryserver/StoreDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imamiako.storediscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
